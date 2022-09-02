e51951b4299e7934b309158280a46fb5c8fb0832
Aplysiida <61487136+Aplysiida@users.noreply.github.com>
2022-09-02 19:07:25 +1200

    Updated report

--- nsga_ga.py
+++ nsga_ga.py
@@ -31,6 +31,9 @@ def read_dataset(dataset_path):
     feature_num = len(data.columns)
     return [data, labels, feature_num]
 
+"""
+Evaluate accuracy/performance of selected features using wrapper fitness
+"""
 def evaluate_accuracy(individual, classifier, data, labels):
     #calc accuracy
     to_drop = [i for i,value in enumerate(individual) if (value == 0)]
@@ -42,12 +45,18 @@ def evaluate_accuracy(individual, classifier, data, labels):
     predicted = classifier.fit(subset_data, labels).predict(subset_data)
     return accuracy_score(labels, predicted)    #calc accuracy
 
+"""
+Evaluate the two objectives' fitness
+"""
 def evaluate_fitness(individual, classifier, data, labels):
     accuracy = evaluate_accuracy(individual, classifier, data, labels)
 
     selected_feature_ratio = float(np.count_nonzero(np.array(individual) == 1))/float(len(individual))
     return (accuracy,selected_feature_ratio)
 
+"""
+Setup the DEAP toolbox for NSGA2
+"""
 def setup_toolbox(rng, data, labels, feature_num):
     toolbox = base.Toolbox()
     #population
@@ -57,13 +66,15 @@ def setup_toolbox(rng, data, labels, feature_num):
 
     #operators
     toolbox.register("evaluate",lambda x : evaluate_fitness(x, KNeighborsClassifier(n_neighbors=5), data, labels))
-    #toolbox.register("evaluate", benchmarks.zdt1)
     toolbox.register("mate",tools.cxOnePoint)
     toolbox.register("mutate",tools.mutFlipBit,indpb=1.0/feature_num)
     toolbox.register("select", tools.selNSGA2)
 
     return toolbox
 
+"""
+Run NSGA2 and return (final front, hypervolume of final front)
+"""
 def run_nsga2(pop_size, max_iter, crossover_rate, mutation_rate):
     #set up population
     pop = toolbox.population(n=pop_size)
@@ -75,8 +86,13 @@ def run_nsga2(pop_size, max_iter, crossover_rate, mutation_rate):
 
     #run generations
     for gen in range(1,max_iter):
+        #select parents
+        children_pop = tools.selTournamentDCD(pop, len(pop))
+        #children_pop = [toolbox.clone(ind) for ind in children_pop]
         #generate children
-        children_pop = algorithms.varAnd(pop, toolbox, cxpb=crossover_rate, mutpb=mutation_rate)
+        children_pop = algorithms.varAnd(children_pop, toolbox, cxpb=crossover_rate, mutpb=mutation_rate)
+        
+        #children_pop = algorithms.varAnd(pop, toolbox, cxpb=crossover_rate, mutpb=mutation_rate)
         #recalculate fitness
         invalid_ind = [ind for ind in children_pop if not ind.fitness.valid]
         fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
@@ -87,8 +103,32 @@ def run_nsga2(pop_size, max_iter, crossover_rate, mutation_rate):
         pop = toolbox.select(pop + children_pop, k=pop_size)
 
     #return nondominated set of solutions/pareto front, hypervolume of front
-    hv = hypervolume(pop,[0.0,1.0])
+    hv = hypervolume(pop,[-0.1,1.1])
     return pop, hv
+
+"""
+Draw scatter plot chart of final front where y= feature ratio and x = classifier accuracy
+"""
+def draw_chart(final_set, data, labels):
+    #calc objectives fitness of solutions
+    x_values = []
+    y_values = []
+    for solution in final_set:
+        #calc x
+        x = evaluate_accuracy(solution, KNeighborsClassifier(n_neighbors=5), data, labels)
+        #calc y
+        y = np.count_nonzero(np.array(solution) == 1)/len(solution)
+        x_values.append(x)
+        y_values.append(y)
+
+    #draw chart of front
+    sns.scatterplot(x=x_values,y=y_values)
+    plt.show()
+
+def get_best_accuracy(solution_set, data, labels):
+    accuracies = [evaluate_accuracy(solution, KNeighborsClassifier(n_neighbors=5), data, labels) for solution in solution_set]
+    return max(accuracies)
+
 
 if __name__=="__main__":
     #store paths as (name of file, path to folder, file format)
@@ -100,7 +140,7 @@ if __name__=="__main__":
     datasets[1][2] = len(datasets[1][0].columns)
 
     #parameters used for each dataset
-    dataset_parameters = [(50,100,1.0,0.3), (10,20,1.0,0.3)]
+    dataset_parameters = [(60,80,1.0,0.3), (300,100,1.0,0.3)]
 
     seeds = np.random.default_rng(seed=1).integers(low=0,high=200,size=3)
 
@@ -109,8 +149,19 @@ if __name__=="__main__":
 
     #for each dataset
     for i,(data, labels, feature_num) in enumerate(datasets):
+        if(i == 0): continue
+        print('i = ',i)
         print('at dataset ',datasets_names[i])
         dataset_parameter = dataset_parameters[i]
+        #calc accuracy of using entire dataset
+        entire_feature_accuracy = evaluate_accuracy(
+            [1 for _ in range(feature_num)], 
+            KNeighborsClassifier(n_neighbors=5), 
+            data, 
+            labels
+        )
+        print('error rate using all features = ',(1.0-entire_feature_accuracy))
+
         for seed in seeds:
             print('seed = ',seed)
             rng = np.random.default_rng(seed=seed)
@@ -121,18 +172,14 @@ if __name__=="__main__":
                 crossover_rate=dataset_parameter[2],
                 mutation_rate=dataset_parameter[3]
             )
-            #calc objectives fitness of solutions
-            x_values = []
-            y_values = []
-            for solution in final_set:
-                #calc x
-                x = evaluate_accuracy(solution, KNeighborsClassifier(n_neighbors=5), data, labels)
-                #calc y
-                y = np.count_nonzero(np.array(solution) == 1)/len(solution)
-                x_values.append(x)
-                y_values.append(y)
+            
+            #calc feature accuracy of best solution
+            best_accuracy = get_best_accuracy(final_set, data, labels)
+            print('best solution error rate = ',(1.0-best_accuracy))
 
-            #draw figure of front
             print('hypervolume of solution = ',hv)
-            sns.scatterplot(x=x_values,y=y_values)
-            plt.show()
+            draw_chart(
+                final_set,
+                data, 
+                labels
+            )
